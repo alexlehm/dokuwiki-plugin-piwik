9deeb75ca4e4b72125dc4e21d7ebf3653ddb9db1
Alexander Lehmann <alexlehm@gmail.com>
2026-01-04 14:18:04 +0100

    remove getInfo()

--- action.php
+++ action.php
@@ -9,21 +9,8 @@ class action_plugin_piwiktagmanager extends ActionPlugin
     public const PWTMHOST = 'PWTMHOST';
 
     /**
-         * return some info
-         */
-    public function getInfo()
-    {
-            return ['author' => 'Alexander Lehmann',
-               'email'  => 'alexlehm@gmail.com',
-               'date'   => '2026-01-02',
-               'name'   => 'Piwik (Matomo) Tag Manager',
-               'desc'   => 'Plugin to embed Piwik/Matomo Tag Manager in your wiki.',
-               'url'    => 'https://wiki.lehmann.cx/projects:dokuwiki_piwik'];
-    }
-
-        /**
-         * Register its handlers with the DokuWiki's event controller
-         */
+      * Register its handlers with the DokuWiki's event controller
+      */
     public function register(EventHandler $controller)
     {
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'addHeaders');
